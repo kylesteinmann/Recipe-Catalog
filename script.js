421c024a178150a669025349218b442a4fdf205f
kylesteinmann <105984447+kylesteinmann@users.noreply.github.com>
2022-08-24 21:04:31 -0500

    Fixed add ingredients button.

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-function addIngredients() {
+let addIngredientsButton = document.querySelector(".addIngredient");
+
+addIngredientsButton.addEventListener("click", () => {
     let numberOfUnits = document.createElement("input");
+    
     numberOfUnits.type = "text";
     numberOfUnits.id = "numberOfUnits";
-    numberOfUnits.className = "col";
+    numberOfUnits.className = "col row";
     numberOfUnits.placeholder = "Number of Units";
 
     let unit = document.createElement("input");
+    
     unit.type = "select";
     unit.id = "unit";
     unit.className = "col";
 
     let ingredient = document.createElement("input");
+    
     ingredient.type = "text";
     ingredient.id = "ingredient";
     ingredient.className = "col";
@@ -21,8 +26,4 @@ function addIngredients() {
     ingredientList.appendChild(numberOfUnits);
     ingredientList.appendChild(unit);
     ingredientList.appendChild(ingredient);
-}; 
-
-let addIngredientsButton = document.querySelector(".addIngredient");
-
-addIngredientsButton.addEventListener("click", addIngredients());
+});
