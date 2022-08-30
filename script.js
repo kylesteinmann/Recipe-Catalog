44a71d26a1c536acae75ef972a69fc6a6eda1a1e
kylesteinmann <kylesteinmann@yahoo.com>
2022-08-30 10:38:31 -0500

    Organized code into functions

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ let recipeMethod = {}
 let recipesArray = JSON.parse(localStorage.getItem("recipes")) || [];
 let recipeForm = document.querySelector(".recipeForm");
 let ingredientIncrement = 0;
+let addRecipeButton = document.querySelector(".addRecipe")
 
-function addTextInputs(){
-    let ingredientKey = "ingredient" + ingredientIncrement;
+
+function pushNameInputToObject() {
     let nameInput = document.querySelector(".recipeName");
     let nameText = nameInput.value;
     recipeMethod.name = nameText;
-
-    if (ingredientIncrement>0) {
+}
+function pushIngredientsInputToObject() {
+ if (ingredientIncrement>0) {
+        let ingredientKey = "ingredient" + ingredientIncrement;
         let numberOfUnitsInput = document.querySelector("#numberOfUnits" + (ingredientIncrement -1));
         let numberOfUnitsText = numberOfUnitsInput.value;
         let unitInput = document.querySelector("#unit" + (ingredientIncrement -1));
@@ -21,51 +24,51 @@ function addTextInputs(){
         let ingredientText = ingredientInput.value; 
         recipeMethod[ingredientKey] = (numberOfUnitsText + " " + unitText + " " + ingredientText);
     }
+}
+
+function addTextInputs(){
+    pushNameInputToObject()
+    pushIngredientsInputToObject()
 };
 
-addIngredientsButton.addEventListener("click", () => {
-    
-// creates next ingredient input boxes when add ingredient button is checked. 
+function createIngredientInputs() {
     let ingredientContainer = document.createElement("div");
-    let numberOfUnits = document.createElement("input");
-    let unit = document.createElement("input");
-    let ingredient = document.createElement("input");
-
     ingredientContainer.className = "ingredientContainer row";
-    
+
+    let numberOfUnits = document.createElement("input");
     numberOfUnits.type = "text";
     numberOfUnits.id = "numberOfUnits"  + ingredientIncrement;
     numberOfUnits.className = "col form-control";
     numberOfUnits.placeholder = "Number of Units";
-       
+    
+    let unit = document.createElement("input");
     unit.type = "text";
     unit.id = "unit"  + ingredientIncrement;
     unit.className = "col form-control";
     unit.placeholder = "Measuring Unit"
-        
+   
+    let ingredient = document.createElement("input");
     ingredient.type = "text";
     ingredient.id = "ingredient"  + ingredientIncrement;
     ingredient.className = "col form-control";
     ingredient.placeholder = "Ingredient";
     
     ingredientList.appendChild(ingredientContainer);
-   
     ingredientContainer.appendChild(numberOfUnits);
     ingredientContainer.appendChild(unit);
     ingredientContainer.appendChild(ingredient);
+}
 
-// adds and updates recipe object when add ingredient button is clicked. 
-    addTextInputs();
+addIngredientsButton.addEventListener("click", () => {
+   
+createIngredientInputs()
+addTextInputs();
     
     ingredientIncrement = ingredientIncrement + 1;
 });
 
-
-let addRecipeButton = document.querySelector(".addRecipe")
-
-//object recipe into array as a method after add recipe is clicked. 
-
 addRecipeButton.addEventListener("click", () => {
+    
     addTextInputs();
 
     recipesArray.push(recipeMethod);
